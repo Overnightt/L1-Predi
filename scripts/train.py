c321517845682ff984839b0ed2aeed1e56c69729
Adrien <adrien.djouder@gmail.com>
2026-01-29 18:26:13 +0100

    la boucle d'entrainement, j'ai reussi a atteindre une precision de 1.0, mais je n'ai pas encore mis en place la sauvegarde du model

---
+++ scripts/train.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from data_check import check_data
+from model import L1_Predictor
+from dataset_builder import build_dataset
+
+n_matches=10
+n_features=40
+model = L1_Predictor(n_matches,n_features)
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(),lr=0.001)
+matches=check_data("../data/season-2425.csv")
+X, Y = build_dataset(matches,n_matches)
+
+epochs=250
+
+for epoch in range(epochs):
+    optimizer.zero_grad()
+    outputs = model(X)
+    loss = loss_function(outputs, Y.squeeze())
+    loss.backward()
+    optimizer.step()
+    preds = torch.argmax(outputs, dim=1)
+    accuracy = (preds == Y.squeeze()).float().mean().item()
+    print(f"Epoch: {epoch}, Loss: {loss.item()}, Accuracy: {accuracy}")
