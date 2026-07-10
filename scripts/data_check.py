27a15aa8a16e4d9aa7e48866f34932c914a02ec0
Adrien <adrien.djouder@gmail.com>
2026-07-10 16:55:03 +0200

    little commit, worked on team values

--- scripts/data_check.py
+++ scripts/data_check.py
@@ -3,7 +3,7 @@ import pandas as pd
 # Loads a CSV file and ensures proper formatting: 
 #    - Converts 'Date' column to datetime 
 #    - Uses ';' as delimiter
-def check_data(filepath):
+def check_data(filepath,date_column="Date"):
     df = pd.read_csv(filepath,delimiter=";")
-    df["Date"] = pd.to_datetime(df["Date"],dayfirst=True)
+    df[date_column] = pd.to_datetime(df[date_column],dayfirst=True)  
     return df
---
+++ scripts/get_squad_stats.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Extract the average age and the squad value from a specific squad, on a specific season 
+def get_squad_stats(team: str, date: pd.Timestamp, teams_table: pd.DataFrame):
+    squad_stats = teams_table[((teams_table["Team"] == team) & (teams_table["SeasonStart"] <= date))]
+    squad_stats = squad_stats.sort_values("SeasonStart",ascending = False)
+    latest_stats = squad_stats.iloc[0]
+    if squad_stats.empty:
+        return None
+    return latest_stats["AvgAge"], latest_stats["SquadValue"]
