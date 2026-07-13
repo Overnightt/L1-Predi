d65ae8933e6b79c58a86b41e4610eed52da77cfe
Adrien <adrien.djouder@gmail.com>
2026-07-13 16:08:41 +0200

    minor corrections

--- scripts/dataset_builder.py
+++ scripts/dataset_builder.py
@@ -1,21 +1,24 @@
 import torch
 import pandas as pd
 from sample_builder import build_sample
+from get_squad_stats import get_squad_stats
+from data_check import check_data
 
-#Builds the full dataset from the matches DataFrame.For each match, it constructs the input tensor (TX,TH2H) 
+#Builds the full dataset from the matches DataFrame.For each match, it constructs the input tensor (TX,T_static_features) 
 #and target (y) using `build_sample`.Skips matches that don't have enough prior history.
 def build_dataset(matches: pd.DataFrame, n: int):
     TX_list = []
-    TH2H_list = []
+    T_static_features_list = []
     y_list  = []
+    squad_values_age = check_data("../data/teams-19-26.csv", date_column="SeasonStart")
     for i in range(len(matches)):
-        sample = build_sample(matches,i,n)
+        sample = build_sample(matches,i,n,squad_values_age)
         if sample is not None:
-            TX ,TH2H , y = sample
+            TX ,T_static_features , y = sample
             TX_list.append(TX)
-            TH2H_list.append(TH2H)
+            T_static_features_list.append(T_static_features)
             y_list.append(y)
     X = torch.stack(TX_list)
-    H = torch.stack(TH2H_list)
+    H = torch.stack(T_static_features_list)
     Y = torch.stack(y_list)
     return X, H, Y
--- scripts/sample_builder.py
+++ scripts/sample_builder.py
@@ -3,36 +3,53 @@ import pandas as pd
 from build_sequences import build_sequence
 from sequence_to_tensor import sequence_to_tensor
 from head_to_head import h2h_score
+from get_squad_stats import get_squad_stats
 
 #Builds a training sample for a single match. Skips matches that don't have enough prior history.
-def build_sample(matches: pd.DataFrame,row : int,n: int):
+def build_sample(matches: pd.DataFrame,row : int,n: int, squad_values_age: pd.DataFrame):
     match= matches.iloc[row]
     date= match["Date"]
     HT= match["HomeTeam"]
     AT= match["AwayTeam"]
+
     seqH = build_sequence(HT,date,matches,n)
     seqA = build_sequence(AT,date,matches,n)
     H2H = h2h_score(HT,AT,date,matches)
     if len(seqH) <n or len(seqA)<n:
         return None
+    home_stats = get_squad_stats(HT, date, squad_values_age)
+    away_stats = get_squad_stats(AT, date, squad_values_age)
+    if home_stats is None or away_stats is None:
+        return None
+    HTA, HTV = home_stats
+    ATA, ATV = away_stats
+
     TH = sequence_to_tensor(seqH)
     TA = sequence_to_tensor(seqA)
     TX = torch.cat((TH,TA),dim=1)
-    TH2H = torch.tensor([H2H]) 
+    T_static_features = torch.tensor([H2H, HTA, HTV, ATA, ATV])
     outcome_map = {"H":0,"D":1,"A":2}
     y = torch.tensor([outcome_map[match["FTR"]]],dtype=torch.long)
-    return TX,TH2H,y
+    return TX,T_static_features,y
 
 #Buils a sample but for prediction
-def build_prediction_sample(matches: pd.DataFrame, home_team: str, away_team: str, n: int):
+def build_prediction_sample(matches: pd.DataFrame, home_team: str, away_team: str, n: int, squad_values_age: pd.DataFrame):
     current_date = matches["Date"].max()
+    
     seqH = build_sequence(home_team, current_date, matches, n)
     seqA = build_sequence(away_team, current_date, matches, n)
-    H2H = h2h_score(home_team,away_team,current_date,matches)
+    H2H = h2h_score(home_team, away_team, current_date, matches)
     if len(seqH) < n or len(seqA) < n:
         return None
+    home_stats = get_squad_stats(home_team, current_date, squad_values_age)
+    away_stats = get_squad_stats(away_team, current_date, squad_values_age)
+    if home_stats is None or away_stats is None:
+        return None
+    HTA, HTV = home_stats
+    ATA, ATV = away_stats
+
     TH = sequence_to_tensor(seqH)
     TA = sequence_to_tensor(seqA)
     TX = torch.cat((TH, TA), dim=1)
-    TH2H = torch.tensor([H2H]) 
-    return TX,TH2H
+    T_static_features = torch.tensor([H2H, HTA, HTV, ATA, ATV])
+    return TX, T_static_features
